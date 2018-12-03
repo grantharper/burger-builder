ef2e758230a6a27ed6b65ebc1583b6103419a95c
grantharper <grant.e.harper@gmail.com>
2018-12-03 17:41:16 -0500

    wired up buger and burger ingredient

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+
+const burger = (props) => {
+  return (
+    <BurgerIngredient type="meat"/>
+  );
+};
+
+export default burger;

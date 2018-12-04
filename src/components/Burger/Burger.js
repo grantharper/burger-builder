47423e8d43ce5a189a77e64f67555f76958c794d
grantharper <grant.e.harper@gmail.com>
2018-12-03 20:27:47 -0500

    add error message when there are no ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,20 @@ import classes from './Burger.css';
 
 const burger = (props) => {
 
-  const transformedIngredients = Object.keys(props.ingredients).map((ingredientKey) => {
+  let transformedIngredients = Object.keys(props.ingredients).map((ingredientKey) => {
     return [...Array(props.ingredients[ingredientKey])].map((elem, index) => {
       return <BurgerIngredient key={ingredientKey + index} type={ingredientKey}/>
     });
-  });
+  })
+  .reduce((arr, el) => {
+    return arr.concat(el);
+  }, []);
 
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
+
+  console.log(transformedIngredients);
   return (
   <div className={classes.Burger}>
     <BurgerIngredient type="bread-top"/>

05c801785f3ed04ad3e8b355fdf707c3c0b15771
grantharper <grant.e.harper@gmail.com>
2018-12-08 14:46:49 -0500

    adjust logo for toolbar and side drawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,13 +4,15 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
-  <header className={classes.Toolbar}>
-    <div>MENU</div>
+<header className={classes.Toolbar}>
+  <div>MENU</div>
+  <div className={classes.Logo}>
     <Logo/>
-    <nav>
-      <NavigationItems/>
-    </nav>
-  </header>
+  </div>
+  <nav>
+    <NavigationItems/>
+  </nav>
+</header>
 );
 
 export default toolbar;

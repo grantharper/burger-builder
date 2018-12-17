3652a13dd38befffba9c66426fd52bb562990687
grantharper <grant.e.harper@gmail.com>
2018-12-17 18:57:18 -0500

    add router to overall app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
+    <BrowserRouter>
       <div>
         <Layout>
-          <BurgerBuilder/>
-          <Checkout/>
+          <Switch>
+            <Route path="/checkout" component={Checkout}/>
+            <Route path="/" component={BurgerBuilder}/>
+          </Switch>
         </Layout>
       </div>
+    </BrowserRouter>
     );
   }
 }

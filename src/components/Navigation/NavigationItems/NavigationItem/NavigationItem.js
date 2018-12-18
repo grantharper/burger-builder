f42ea13709e3e5960de33db6a7a94e6acd75d5c1
grantharper <grant.e.harper@gmail.com>
2018-12-17 20:08:10 -0500

    add checkout link to navigate to checkout

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import classes from './NavigationItem.css';
+import {Link} from 'react-router-dom';
 
 const navigationItem = (props) => (
 <li className={classes.NavigationItem}>
-  <a
-  href={props.link}
-  className={props.active ? classes.active : null}
-  >{props.children}</a>
+  <Link to={props.link}>{props.children}</Link>
+  {/*<a*/}
+  {/*href={props.link}*/}
+  {/*className={props.active ? classes.active : null}*/}
+  {/*>{props.children}</a>*/}
 </li>
 );
 

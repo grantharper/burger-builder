be88b2f9bbf51df9b13ef5b9a61a84c672e612e2
grantharper <grant.e.harper@gmail.com>
2018-12-30 20:53:08 -0500

    only run redux dev tools in development environment

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import thunk from "redux-thunk";
 import {BrowserRouter} from "react-router-dom";
 import Provider from "react-redux/es/components/Provider";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE : null)|| compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,

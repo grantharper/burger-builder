6e64a612ef443e6423b7ae0ba8abbd732287b801
grantharper <grant.e.harper@gmail.com>
2018-12-30 21:19:01 -0500

    lazy load orders route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, {Component} from 'react';
+import React, {Component, Suspense} from 'react';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
+// import Orders from './containers/Orders/Orders';
 import {Switch, Route, withRouter, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import Auth from './containers/Auth/Auth';
 import Logout from "./containers/Auth/Logout/Logout";
 import * as actionCreators from './store/actions/index';
 
+const Orders = React.lazy(() => import('./containers/Orders/Orders'))
 
 class App extends Component {
 
@@ -19,9 +20,11 @@ class App extends Component {
   render() {
 
     let authRoutes = Array.of(
-      <Route key="/orders" path="/checkout" component={Checkout}/>,
-      <Route key="/orders" path="/orders" component={Orders}/>,
-      <Route key="/logout" path="/logout" component={Logout}/>
+      <Route key="/checkout" path="/checkout" component={Checkout}/>,
+      <Route key="/orders" path="/orders" render={() => <Suspense fallback={<div>Loading...</div>}><Orders/></Suspense>} />,
+      <Route key="/logout" path="/logout" component={Logout}/>,
+      <Route key="/" path="/" exact component={BurgerBuilder}/>,
+      <Redirect key="redirect" to="/" />
     );
 
     let publicRoutes = Array.of(

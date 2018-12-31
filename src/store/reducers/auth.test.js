8267999b50e5949669e54d4d246f8c6cee7631ec
grantharper <grant.e.harper@gmail.com>
2018-12-31 12:02:58 -0500

    create a test for auth success action

--- src/store/reducers/auth.test.js
+++ src/store/reducers/auth.test.js
@@ -3,16 +3,31 @@ import * as actionTypes from '../actions/actionTypes';
 
 describe('auth reducer', () => {
 
+  const initialState = {
+    token: null,
+    userId: null,
+    error: null,
+    loading: false
+  };
+
   it('should return the initial state when no state is supplied and default action', () => {
 
-    const initialState = {
-      token: null,
-      userId: null,
+    expect(reducer(undefined, {})).toEqual(initialState);
+
+  });
+
+
+  it('should store the token upon initial login', () => {
+
+    const stateWithToken = {
+      ...initialState,
+      token: 'blah',
+      loading: false,
       error: null,
-      loading: false
+      userId: 'user'
     };
 
-    expect(reducer(undefined, {})).toEqual(initialState);
+    expect(reducer(initialState, {type: actionTypes.AUTH_SUCCESS, token: 'blah', userId: 'user'})).toEqual(stateWithToken)
 
   });
 
